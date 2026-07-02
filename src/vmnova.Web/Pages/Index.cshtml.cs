d0aa7f3d368ea0f682c31d06c6ff4699fed486c1
masaftic <yousefalaa618y@gmail.com>
2026-07-03 00:17:32 +0300

    init project structure

---
+++ src/vmnova.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace vmnova.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
